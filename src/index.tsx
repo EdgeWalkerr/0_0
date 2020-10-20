8419053aa4a94abe4bae5703f1544234a6a069c2
shenxiang.feng <shenxiang.feng@binance.com>
2020-10-20 14:37:13 +0800

    add equalFn

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,20 @@ import React, {
 	useState,
 	useRef,
 	useEffect,
-	useCallback
+	useCallback,
+	ReactNode
 } from "react";
 import compareDeepSetShallow from "./compareDeepSetShallow";
 type ISelector<T, U> = (
 	selector: (state: U) => T,
 	equalFn?: (obj1: T, obj2: T) => boolean
 ) => any;
+
+function usePersistRef<T>(value: T) {
+	const ref = useRef(null as any);
+	ref.current = value;
+	return ref;
+}
 
 const isEqual = (obj1: any, obj2: any) =>
 	compareDeepSetShallow(obj1, obj2) === obj1;
@@ -21,22 +28,20 @@ const noop = () => { };
 const selector = (
 	valueRef: React.MutableRefObject<any>,
 	listenerListRef: React.MutableRefObject<IListener[]>
-): ISelector<any, any> => (selector: any, equalFn: any) => {
+): ISelector<any, any> => (selector: any, equalFn = isEqual) => {
 	const [, setAccumulator] = useState(0);
 	const forceUpdate = useCallback(() => {
 		setAccumulator((n) => n + 1);
 	}, []);
 	const stateRef = useRef(selector(valueRef.current));
-	const selectorRef = useRef(noop as any);
-	selectorRef.current = selector;
-	const equalFnRef = useRef(noop as any);
-	equalFnRef.current = equalFn;
+	const selectorRef = usePersistRef(selector);
+	const equalFnRef = usePersistRef(equalFn);
 	useEffect(() => {
 		const num = listenerListRef.current.length;
 		listenerListRef.current[num] = (state) => {
 			const newState = selectorRef.current(state);
-			const newEqaulFn = equalFnRef.current || isEqual;
-			if (!newEqaulFn(stateRef.current, newState)) {
+			const newEqualFn = equalFnRef.current || isEqual;
+			if (!newEqualFn(stateRef.current, newState)) {
 				stateRef.current = newState;
 				forceUpdate();
 			}
@@ -44,23 +49,32 @@ const selector = (
 		return () => {
 			listenerListRef.current[num] = noop;
 		};
-	}, [forceUpdate, equalFn]);
+	}, [forceUpdate, equalFn, equalFnRef, selectorRef]);
 	return stateRef.current;
 };
 
 type IListener = (state: any) => any;
 
-export function Provider({ children, value }: any) {
+export function Provider({
+	children,
+	value,
+	equalFn = isEqual
+}: {
+	children: ReactNode;
+	value: any;
+	equalFn?: (obj1: any, obj2: any) => boolean;
+}) {
 	const valueRef = useRef(value);
 	const listenerListRef = useRef([] as IListener[]);
+	const equalFnRef = usePersistRef(equalFn);
 	useEffect(() => {
-		if (compareDeepSetShallow(valueRef.current, value) !== valueRef.current) {
+		if (!equalFnRef.current(valueRef.current, value)) {
 			valueRef.current = value;
 			listenerListRef.current.forEach((listener) => {
 				listener(valueRef.current);
 			});
 		}
-	}, [value]);
+	}, [value, equalFnRef]);
 	const useSelector = useMemo(() => selector(valueRef, listenerListRef), []);
 	return useMemo(
 		() => <Context.Provider value={useSelector}>{children}</Context.Provider>,
