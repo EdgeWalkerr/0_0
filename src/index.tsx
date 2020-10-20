18a671be2581203e28c40f467cf5ae72b5d764b8
shenxiang.feng <shenxiang.feng@binance.com>
2020-10-20 15:10:04 +0800

    add multiple type for useSelector

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,9 @@ import React, {
 	ReactNode
 } from "react";
 import compareDeepSetShallow from "./compareDeepSetShallow";
-type ISelector<T, U> = (
-	selector: (state: U) => T,
-	equalFn?: (obj1: T, obj2: T) => boolean
+type ISelector = (
+	selector: atomType | atomType[] | ((state: any) => any),
+	equalFn?: (obj1: any, obj2: any) => boolean
 ) => any;
 
 function usePersistRef<T>(value: T) {
@@ -23,35 +23,48 @@ function usePersistRef<T>(value: T) {
 const isEqual = (obj1: any, obj2: any) =>
 	compareDeepSetShallow(obj1, obj2) === obj1;
 
-const Context = createContext((() => ({})) as ISelector<any, any>);
+type atomType = string | number;
+
+const Context = createContext((() => ({})) as ISelector);
 const noop = () => { };
 const selector = (
 	valueRef: React.MutableRefObject<any>,
 	listenerListRef: React.MutableRefObject<IListener[]>
-): ISelector<any, any> => (selector: any, equalFn = isEqual) => {
-	const [, setAccumulator] = useState(0);
-	const forceUpdate = useCallback(() => {
-		setAccumulator((n) => n + 1);
-	}, []);
-	const stateRef = useRef(selector(valueRef.current));
-	const selectorRef = usePersistRef(selector);
-	const equalFnRef = usePersistRef(equalFn);
-	useEffect(() => {
-		const num = listenerListRef.current.length;
-		listenerListRef.current[num] = (state) => {
-			const newState = selectorRef.current(state);
-			const newEqualFn = equalFnRef.current || isEqual;
-			if (!newEqualFn(stateRef.current, newState)) {
-				stateRef.current = newState;
-				forceUpdate();
-			}
-		};
-		return () => {
-			listenerListRef.current[num] = noop;
-		};
-	}, [forceUpdate, equalFn, equalFnRef, selectorRef]);
-	return stateRef.current;
-};
+): ISelector => (
+	selector: atomType | atomType[] | ((state: any) => any),
+	equalFn = isEqual
+) => {
+		const [, setAccumulator] = useState(0);
+		const forceUpdate = useCallback(() => {
+			setAccumulator((n) => n + 1);
+		}, []);
+		const selectorRef = usePersistRef(
+			typeof selector === "function"
+				? selector
+				: (state: any) =>
+					(typeof selector === "string" || typeof selector === "number"
+						? [selector]
+						: selector
+					).reduce((result, key) => state?.[key], state)
+		);
+		const stateRef = useRef(selectorRef.current(valueRef.current));
+		const equalFnRef = usePersistRef(equalFn);
+		useEffect(() => {
+			const num = listenerListRef.current.length;
+			listenerListRef.current[num] = (state) => {
+				const newState = selectorRef.current(state);
+				const newEqualFn = equalFnRef.current || isEqual;
+				if (!newEqualFn(stateRef.current, newState)) {
+					stateRef.current = newState;
+					forceUpdate();
+				}
+			};
+			return () => {
+				listenerListRef.current[num] = noop;
+			};
+		}, [forceUpdate, equalFn, equalFnRef, selectorRef]);
+		return stateRef.current;
+	};
 
 type IListener = (state: any) => any;
 
@@ -82,14 +95,11 @@ export function Provider({
 	);
 }
 
-export const useSelector: ISelector<any, any> = (
-	selector,
-	equalFn = isEqual
-) => {
+export const useSelector: ISelector = (selector, equalFn = isEqual) => {
 	return useContext(Context)(selector, equalFn);
 };
 
-export const connect: ISelector<any, any> = (selector, equalFn = isEqual) => (
+export const connect: ISelector = (selector, equalFn = isEqual) => (
 	Component: any
 ): any => {
 	return function Consumer<T>(props: T) {
